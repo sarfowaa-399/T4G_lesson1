47340a8d18c4e6a6dac914f322ced6f988c41881
Your Name <amasarfowaap@gmail.com>
2024-11-25 13:35:04 +0000

    i made changes in my index.html file and added files in my index.js file

---
+++ src/index.js
@@ -0,0 +1,21 @@
+alert("Hello World");
+alert("Ama Sarfowaa");
+
+let name;
+prompt("What is your name");
+
+let sardinebox = prompt("How many sardine boxes do u want to buy");
+alert(sardinebox);
+
+let sardine = prompt("how many sardine boxes do u want to buy ");
+alert(sardine + " it is");
+
+let friend = prompt("what is the name of your friend");
+let age = prompt("what is her age");
+let = prompt("what is her favorite color?");
+
+alert("my friend is" + friend +" she is " + age + "years" +" and her favorite color is " + color);
+
+
+
+alert(`my friend name is ${friend} she is ${age} years old and her favorite color is ${color}`);
